9a99b9f2e0e91b9a005ecf8f1e8441c5525a9fec
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-02 22:56:06 -0500

    Add an extension for Notification Name

---
+++ Weather/NotificationName.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName.swift
+//  Weather
+//
+//  Created by Ilgar Ilyasov on 6/2/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didSaveUserLocation = Notification.Name("didSaveUserLocation")
+}
