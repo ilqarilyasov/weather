5e4aec4055c47ca204db3ca2343545080926dcaf
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-02 22:53:00 -0500

    Create Coordinate model object

---
+++ Weather/Models/Coordinate.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinate.swift
+//  Weather
+//
+//  Created by Ilgar Ilyasov on 6/1/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinate: Codable {
+    let lat: Double
+    let lon: Double
+}
